91736bd79b8870e8b87cd7790ade3c3c836a7220
radelmann <robadelmann@gmail.com>
2016-05-07 09:03:40 -0700

    local display state for comment list item

--- client/src/views/comment_list_item.js
+++ client/src/views/comment_list_item.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { SHOW_ALL, MINIMIZE_ALL, TOGGLE_ALL } from '../actions/types';
 
 export default class CommentListItem extends Component {
   constructor(props) {
     super(props);
 
-    //initialize as not minimized
     this.state = { minimized : false };
   }
 
   toggle() {
-    this.setState({minimized : !this.state.minimized});
+    this.setState({ minimized : !this.state.minimized });
   }
 
   getClassName() {
@@ -18,13 +18,18 @@ export default class CommentListItem extends Component {
   }
 
   render() {
-    const { title, message } = this.props;
-    
+    const { title, message, display } = this.props;
+    const className = this.getClassName();
+ 
     return (
-      <li className={this.getClassName()} onClick={this.toggle.bind(this)}>
-        <div className="bold">{title}</div>
-        <div>{message}</div>
+      <li className={className} onClick={this.toggle.bind(this)}>
+        <div className="title">{title}</div>
+        <div className="message">{message}</div>
       </li>
     );
+  }
+
+  componentDidMount(){
+    this.setState({ override : false });
   }
 }
