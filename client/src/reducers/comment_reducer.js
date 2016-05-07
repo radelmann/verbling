e5058bb2cad87f8218974c5d132e8d0fcc6563e7
radelmann <robadelmann@gmail.com>
2016-05-07 08:54:21 -0700

    handle fetch comments action type

--- client/src/reducers/comment_reducer.js
+++ client/src/reducers/comment_reducer.js
@@ -1,7 +1,9 @@
-import { POST_COMMENT } from '../actions/types';
+import { FETCH_COMMENTS, POST_COMMENT } from '../actions/types';
 
 export default function(state = [], action) {
   switch(action.type) {
+     case FETCH_COMMENTS:
+      return action.payload;
     case POST_COMMENT:
       return [ ...state, action.payload];
     default:
