29159269cb4fc1854e2b40979603272ecfeeda4b
radelmann <robadelmann@gmail.com>
2016-05-07 10:43:26 -0700

    Deleted display reducer, add minimized property to comments reducer handle all display actions through comments reducer

--- client/src/reducers/comment_reducer.js
+++ client/src/reducers/comment_reducer.js
@@ -1,11 +1,39 @@
-import { FETCH_COMMENTS, POST_COMMENT } from '../actions/types';
+import { FETCH_COMMENTS, POST_COMMENT, SHOW_ALL, MINIMIZE_ALL, TOGGLE_ALL, TOGGLE_COMMENT } from '../actions/types';
 
 export default function(state = [], action) {
   switch(action.type) {
      case FETCH_COMMENTS:
-      return action.payload;
+      //get all comments, initialize minimized property
+      return action.payload.map(comment => {
+        comment.minimized=false; 
+        return comment;
+      });
     case POST_COMMENT:
+      //add minimized property,initialize to false
+      action.payload.minimized = false;
       return [ ...state, action.payload];
+    case SHOW_ALL:
+      return state.map(comment => {
+        comment.minimized=false; 
+        return comment;
+      });
+    case MINIMIZE_ALL:
+      return state.map(comment => {
+        comment.minimized=true; 
+        return comment;
+      });
+    case TOGGLE_ALL:
+      return state.map(comment => {
+        comment.minimized = !comment.minimized; 
+        return comment;
+      });
+    case TOGGLE_COMMENT:
+      return state.map(comment => {
+        if (comment._id === action.payload.id) {
+          comment.minimized = !comment.minimized; 
+        }
+        return comment;
+      });
     default:
       return state;
   }
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import CommentReducer from './comment_reducer';
 import ModalReducer from './modal_reducer';
-import DisplayReducer from './display_reducer';
 
 const rootReducer = combineReducers({
   form,
   comments: CommentReducer,
-  modal: ModalReducer,
-  display: DisplayReducer
+  modal: ModalReducer
 });
 
 export default rootReducer;
