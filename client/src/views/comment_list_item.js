8ffa96a49a92965eef7a6e9feb77906b61767c57
radelmann <robadelmann@gmail.com>
2016-05-06 21:23:55 -0700

    added functionality for toggling list items individually

--- client/src/views/comment_list_item.js
+++ client/src/views/comment_list_item.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export default class CommentListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    //initialize as not minimized
+    this.state = { minimized : false };
+  }
+
+  toggle() {
+    this.setState({minimized : !this.state.minimized});
+  }
+
+  getClassName() {
+    return 'comment ' + ((this.state.minimized) ? 'minimized' : '');     
+  }
+
   render() {
     const { title, message } = this.props;
+    
     return (
-      <li className="comment" onClick={this.onClick.bind(this)}>
+      <li className={this.getClassName()} onClick={this.toggle.bind(this)}>
         <div className="bold">{title}</div>
         <div>{message}</div>
       </li>
