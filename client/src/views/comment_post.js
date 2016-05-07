3e04f1b135334070f0d179cc268c3858a9448c8e
radelmann <robadelmann@gmail.com>
2016-05-06 18:23:18 -0700

    modal form for posting comments

---
+++ client/src/views/comment_post.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { postComment, resetForm, hideModal } from '../actions/index';
+import Modal from 'react-modal';
+
+const customStyles = {
+  overlay : {
+    backgroundColor : 'rgba(75, 75, 75, .75)'
+  },
+  content : {
+    border : 'none',
+    background  : 'transparent'
+  }
+};
+
+class CommentPost extends Component {
+  onSubmit(props) {
+    this.props.hideModal();
+    this.props.postComment(props);
+  }
+
+  closeModal() {
+    this.props.resetForm('CommentPostForm');
+    this.props.hideModal();
+  }
+
+  render() {
+    const { fields: {message}, handleSubmit, modal } = this.props;
+
+    return (
+      <div>
+        <div>
+          <Modal
+            isOpen={modal}
+            onRequestClose={this.closeModal.bind(this)}
+            style={customStyles} >
+            <div className="close-form-container">
+              <button className="transparent align-right white" onClick={this.closeModal.bind(this)}>X</button>
+            </div> 
+            <form className="post-form" onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
+                <div className="form-group">
+                  <textarea className="form-control" {...message} rows="5" placeholder="what do you want to say?"/>
+                  { message.touched && message.error && <div className="error">{message.error}</div> }
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+          </Modal>
+        </div>
+      </div>
+    );
+  }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.message) {
+    errors.message = 'Enter a message.';
+  }
+
+  return errors;
+}
+
+function mapStateToProps(state) {
+  return { modal: state.modal };
+}
+
+export default reduxForm({
+  form: 'CommentPostForm',
+  fields: ['message'],
+  validate,
+},mapStateToProps,{postComment, resetForm, hideModal})(CommentPost);
