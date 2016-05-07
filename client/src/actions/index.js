66e5f8db3dfad2e8f4aba19b1cfe45764aafcccc
radelmann <robadelmann@gmail.com>
2016-05-07 08:31:21 -0700

    added display action creators and types

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,4 @@
-import { POST_COMMENT, SHOW_MODAL, HIDE_MODAL } from './types';
+import { SHOW_ALL, MINIMIZE_ALL, TOGGLE_ALL, POST_COMMENT, SHOW_MODAL, HIDE_MODAL } from './types';
 import { reset } from 'redux-form';
 
 export function postComment(comment) {
@@ -23,5 +23,23 @@ export function hideModal() {
 export function resetForm(formName) {
   return function(dispatch) { 
     dispatch(reset(formName));  
+  }
+}
+
+export function showAll() {
+  return {
+    type: SHOW_ALL
+  }
+}
+
+export function minimizeAll() {
+  return {
+    type: MINIMIZE_ALL
+  }
+}
+
+export function toggleAll() {
+  return {
+    type: TOGGLE_ALL
   }
 }
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,6 @@
+export const SHOW_ALL = 'show_all';
+export const MINIMIZE_ALL = 'minimize_all';
+export const TOGGLE_ALL = 'toggle_all';
 export const POST_COMMENT  = 'post_comment';
 export const SHOW_MODAL = 'show_modal';
 export const HIDE_MODAL = 'hide_modal';
