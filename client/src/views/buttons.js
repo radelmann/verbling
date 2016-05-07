d8d077ab743c34617230b3a68fbdbb4dcb232c08
radelmann <robadelmann@gmail.com>
2016-05-06 18:22:51 -0700

    buttons view

---
+++ client/src/views/buttons.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { showModal, resetForm } from '../actions/index';
+
+class Buttons extends Component {
+  openModal() {
+    // this.props.resetForm('CommentPostForm');
+    this.props.showModal();
+  }
+  render() {
+    return (
+      <div>
+        <button className="compose-button" onClick={this.openModal.bind(this)}>post comment</button>        
+      </div>
+    );
+  }
+}
+
+export default connect(null,{ showModal, resetForm })(Buttons);
