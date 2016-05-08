b53c4b52a09a7de692baaa87dc4d0d410d47b148
radelmann <robadelmann@gmail.com>
2016-05-07 17:56:34 -0700

    comment post form code cleanup

--- client/src/views/comment_post.js
+++ client/src/views/comment_post.js
@@ -28,27 +28,23 @@ class CommentPost extends Component {
     const { fields: {title, message}, handleSubmit, modal } = this.props;
 
     return (
-      <div>
-        <div>
-          <Modal
-            isOpen={modal}
-            onRequestClose={this.closeModal.bind(this)}
-            style={customStyles} >
-            <div className="close-form-container">
-              <button className="transparent align-right white" onClick={this.closeModal.bind(this)}>X</button>
-            </div> 
-            <form className="post-form" onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
-                <div className="form-group">
-                  <input className="form-control" {...title} placeholder="title"/>
-                  { title.touched && title.error && <div className="error">{title.error}</div> }
-                  <textarea className="form-control" {...message} rows="5" placeholder="message"/>
-                  { message.touched && message.error && <div className="error">{message.error}</div> }
-                </div>
-                <button type="submit" className="btn btn-primary submit">Submit</button>
-            </form>
-          </Modal>
-        </div>
-      </div>
+      <Modal
+        isOpen={modal}
+        onRequestClose={this.closeModal.bind(this)}
+        style={customStyles} >
+        <div className="close-form-container">
+          <button className="transparent align-right white" onClick={this.closeModal.bind(this)}>X</button>
+        </div> 
+        <form className="post-form" onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
+            <div className="form-group">
+              <input className="form-control" {...title} placeholder="title"/>
+              { title.touched && title.error && <div className="error">{title.error}</div> }
+              <textarea className="form-control" {...message} rows="5" placeholder="message"/>
+              { message.touched && message.error && <div className="error">{message.error}</div> }
+            </div>
+            <button type="submit" className="btn btn-primary submit">Submit</button>
+        </form>
+      </Modal>
     );
   }
 }
